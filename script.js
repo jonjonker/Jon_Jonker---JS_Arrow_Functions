b9b90b3fb2b024029b33cb25c5c97b5dbff8866b
Jon Jonker <jonjonker@hotmail.com>
2020-12-22 21:59:50 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+// deel A
+
+const ikRockArrowFunctions = () => {
+    console.log("Joe, ik rock de arrow functions!");
+};
+
+ikRockArrowFunctions();
+
+// deel B
+
+const fivePlusSeven = () => 5 + 7
+
+fivePlusSeven();
+
+// deel C
+
+const myFunction = () => a + b
+
+myFunction();
+
+// deel D
+
+const addFive = a => a + 5
+
+addFive ();
+
+// deel E
+
+const createObject = () => ({ greeting: "hoi" })
+
+createObject(); 
